7aea8039bc1d83dba143855f4c6a3d1aa0ab9d8f
Afrin Nahar <afrinnahar1999@gmail.com>
2024-02-14 10:58:56 +0600

    html and css data sent to the server as json

--- src/panels.ts
+++ src/panels.ts
@@ -15,48 +15,37 @@ export default (editor: Editor, opts: RequiredPluginOptions) => {
   });
   const btn: any = editor.Panels.getButton("options", "publishSite");
   btn.on("change", async () => {
-    // Get current project data
-    const projectData = editor.getProjectData();
-    // Load project data
-    const data = editor.loadProjectData(projectData);
-    // console.log(data.pages);
+    // get html  & css from the editor
+    const html = editor.getHtml();
+    const css = editor.getCss();
 
-    // Store data
-    // const storedProjectData = await editor.store({
-    //   data: { html, css },
-    // });
-
-    // // Load data
-    // const loadedProjectData = await editor.load();
-
-    // //   editor.Storage.add('remote', {
-    // //       store(data, options) {
-    // //           return {data}
-    // //       },
-    // //   })
-    // console.log(storedProjectData);
+    // get the Id from the location
     //   const url = document.location.href;
-
     const url = "https://chepapest.com/admin/landing_page/edit/1";
     const id = url.split("/").pop();
+    const dataObj = {
+      id,
+      html: `${html}`,
+      css: `${css}`,
+    };
 
     const projectEndpoint = `https://chepapest.com/api/dev/user/landing-page/${id}/save`;
 
-    editor.Commands.add("publishSite", {
-      run: function (editor, sender) {
-        sender && sender.set("active", 0); // turn off the button
-        editor.store();
-        console.log(editor.store());
+    // Send data
+    async function sendData() {
+      const response = await fetch(projectEndpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(dataObj),
+        // for redirecting to another page
+        // redirect: 'follow'
+      });
+      const res = await response.json();
+      console.log(res);
+    }
 
-        var htmldata = editor.getHtml();
-        var cssdata = editor.getCss();
-        $.post(projectEndpoint, {
-          html: htmldata,
-          css: cssdata,
-        });
-      },
-    });
-
-    // console.log(data);
+    sendData();
   });
 };
