9b3dd25e0abc999b16ee32f2a04596780ca9b9f8
Afrin Nahar <afrinnahar1999@gmail.com>
2024-02-14 15:39:30 +0600

    Added dist folder

---
+++ dist/index.d.ts
@@ -0,0 +1,28 @@
+import { Plugin } from 'grapesjs';
+
+export type PluginOptions = {
+	/**
+	 * Inecessary plugin options
+	 * @default 'api'
+	 * @default 'API'
+	 * @default ''
+	 * @default ''
+	 * @default 'api'
+	 */
+	id?: string[];
+	label?: string[];
+	style?: string;
+	block?: (blockId: string) => {};
+	styleAdditional?: string;
+	classPrefix?: string;
+	blocks?: string[];
+	token?: any;
+};
+export type RequiredPluginOptions = Required<PluginOptions>;
+declare const plugin: Plugin<PluginOptions>;
+
+export {
+	plugin as default,
+};
+
+export {};
