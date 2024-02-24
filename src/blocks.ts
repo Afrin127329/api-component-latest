5fd6d79c9ea7abfa7e931ac3915c10a604659bb2
Afrin Nahar <afrinnahar1999@gmail.com>
2024-02-24 11:11:44 +0600

    removed unnecessary files

--- src/blocks.ts
+++ src/blocks.ts
@@ -679,7 +679,6 @@ export default async (editor: Editor, opts: RequiredPluginOptions) => {
   // Tabs component using bootstrap
   editor.BlockManager.add("tabs", {
     label: "Tabs",
-    category: "Extra",
     media: `<svg class="bi bi-segmented-nav" viewBox="0 0 16 16">
     <path d="M0 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6 3h4V5H6zm9-1V6a1 1 0 0 0-1-1h-3v4h3a1 1 0 0 0 1-1"/>
   </svg>`,
