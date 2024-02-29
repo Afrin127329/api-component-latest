f5673fbe82e10ca3b96fbb2398140cc5c639c4c7
Afrin Nahar <afrinnahar1999@gmail.com>
2024-02-29 15:24:13 +0600

    added footer bootstrap

--- src/blocks.ts
+++ src/blocks.ts
@@ -763,6 +763,75 @@ export default async (editor: Editor, opts: RequiredPluginOptions) => {
     `,
   });
 
+  // Footer with bootstrap
+  editor.BlockManager.add("footer", {
+    label: "Footer",
+    category: "Extra",
+    media: `<svg class="bi bi-segmented-nav" viewBox="0 0 16 16">
+    <path d="M0 6a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v2a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm6 3h4V5H6zm9-1V6a1 1 0 0 0-1-1h-3v4h3a1 1 0 0 0 1-1"/>
+  </svg>`,
+    content: `
+    <footer class="w-100 py-4 flex-shrink-0">
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <h5 class="h1 text-white">FB.</h5>
+                <p class="small text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+                <p class="small text-muted mb-0">&copy; Copyrights. All rights reserved. <a class="text-primary" href="#">Bootstrapious.com</a></p>
+            </div>
+            <div class="col-lg-2 col-md-6">
+                <h5 class="text-white mb-3">Quick links</h5>
+                <ul class="list-unstyled text-muted">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Get started</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-2 col-md-6">
+                <h5 class="text-white mb-3">Quick links</h5>
+                <ul class="list-unstyled text-muted">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Get started</a></li>
+                    <li><a href="#">FAQ</a></li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <h5 class="text-white mb-3">Newsletter</h5>
+                <p class="small  text-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+  <style>
+
+  .form-control {
+    background: #212529;
+    border-color: #545454;
+  }
+  
+  .form-control:focus {
+    background: #212529;
+  }
+  
+  footer {
+    background: #212529;
+  }
+  a {
+    color: white;
+    text-decoration: none;
+    transition: all 0.3s;
+  }
+  
+  a:hover, a:focus {
+    text-decoration: none;
+  }
+  </style>
+    `,
+  });
+
   // Vertically centered Modal Active component using bootstrap --> Will add later
   //   editor.BlockManager.add("bootstrapModal", {
   //     label: "Modal",
