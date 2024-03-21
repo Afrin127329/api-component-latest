afeb1b458ac387e7631a25688c548e22959368de
Afrin Nahar <afrinnahar1999@gmail.com>
2024-03-21 17:06:19 +0600

    v1

--- src/components.ts
+++ src/components.ts
@@ -108,18 +108,21 @@ export default (editor: Editor, opts: RequiredPluginOptions) => {
         this.on("change:attributes:selectedData", () => {
           // Take Selected Data object from the traits
           const data = this.getAttributes().selectedData;
-          // console.log(data);
 
           // Testing
           const wrapper: any = editor.getWrapper();
-          const idEl = wrapper.find(".productId")[0].view;
-          const priceEl = wrapper.find(".productPrice")[0].view;
-          const qntyEl = wrapper.find(".productQuantity")[0].view;
+          const idEl = wrapper.find(".productId")[0].view?.attr;
+          const priceEl = wrapper.find(".productPrice")[0].view?.attr;
+          const qntyEl = wrapper.find(".productQuantity")[0].view?.attr;
           console.log(idEl, priceEl, qntyEl);
 
-          idEl.el.innerHTML = data.id;
-          priceEl.el.innerHTML = data.price;
-          qntyEl.el.innerHTML = 1;
+          idEl.value = data.id;
+          priceEl.value = data.price;
+          qntyEl.value = 1;
+
+          // idEl.el.innerHTML = data.id;
+          // priceEl.el.innerHTML = data.price;
+          // qntyEl.el.innerHTML = 1;
 
           // Grab the DOM Elements
           // const idElem: any = Components.getById("productId").view;
